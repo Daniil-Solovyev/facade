8c66fc1c45f5b22562a7096163b8231903cb7ffa
Daniil <vincente.pooh@gmail.com>
2022-12-26 11:54:11 +0300

    facade

---
+++ app/Services/RedisSettingsService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class RedisSettingsService
+{
+    public function settings()
+    {
+        return 'Here is my redis settings';
+    }
+
+    public function anotherMethod()
+    {
+        return 'Here is my redis another method';
+    }
+}
